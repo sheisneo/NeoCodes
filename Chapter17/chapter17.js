17f9224b17b4426da4341f13b25838f04211e0b7
Neo <139254886+sheisneo@users.noreply.github.com>
2024-03-04 20:51:12 +0200

    Add files via upload

---
+++ Chapter17/chapter17.js
@@ -0,0 +1,24 @@
+let days = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+
+ let weather = ["Sunny", "Partly Sunny", "Partly Cloudy", 
+"Cloudy", "Raining", "Snowing", "Thunderstorm", 
+"Foggy"];
+
+minTemp = 0;
+maxTemp = 100;
+
+generateWeather();
+ function generateWeather() {
+  for (var i = 0; i < days.length; i++) {
+    var weatherToday = weather[Math.floor(Math.random() * 
+weather.length)];
+    var tempToday = Math.floor(Math.random() * (maxTemp -minTemp) + minTemp);
+    document.getElementById("5DayWeather").innerHTML += 
+ "<div id='" + days[i] + "' class='" + 
+weatherToday + "'><b>Forecast for " + days[i] + 
+":</b><br><br>" + weatherToday + " and " + 
+tempToday + " degrees.</div>";
+  }
+ }
+
